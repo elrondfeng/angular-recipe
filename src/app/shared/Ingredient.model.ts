c7321aa7bf6ef03c7f3ddc636434ca5b8308d630
Tao Feng <elrondfeng@gmail.com>
2020-06-14 18:23:04 -0400

    shopping list

---
+++ src/app/shared/Ingredient.model.ts
@@ -0,0 +1,9 @@
+
+// here is the shortcut
+// this is same as define the public name and public number first, then assign them the passed in value in the constructor method
+
+export class Ingredient {
+  constructor(public name: string, public amount: number) {
+  }
+}
+
